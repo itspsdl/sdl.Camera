4bc2ec47a11e65399bece8bdf950f4ddc63a779c
YutaTakayanagi <takayanagi.y.aa@m.titech.ac.jp>
2021-05-06 14:39:33 +0900

    課題3

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -5,19 +5,29 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+    private String currentPhotoPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
         Button photoButton = findViewById(R.id.photo_button);
         photoButton.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            // TODO: You should setup appropriate parameters for the intent
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
             PackageManager manager = getPackageManager();
             @SuppressLint("QueryPermissionsNeeded")
             List<ResolveInfo> activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
             if (!activities.isEmpty()) {
-                startActivityForResult(intent, REQ_PHOTO);
+                /*File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+
+                }
+
+                if(photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);*/
+                    startActivityForResult(intent, REQ_PHOTO);
+                //}
             } else {
                 Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    /* private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyMMdd__HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        currentPhotoPath = image.getAbsolutePath(); // 画像のパスを保存
+        return image;
+    }*/
 
     private void showPhoto() {
         if (photoImage == null) {
@@ -53,7 +83,10 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                // TODO: You should implement the code that retrieve a bitmap image
+                photoImage = (Bitmap) data.getExtras().get("data");
+                /* if (currentPhotoPath == null) {
+                    photoImage = BitmapFactory.decodeFile(currentPhotoPath);
+                } */
             }
         }
     }
